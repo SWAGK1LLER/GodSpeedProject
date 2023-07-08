e8fcf8d4898549ad11a0817a07e8c10ddaf066ac
Daniel Haselock <daniel.haselock@gmail.com>
2023-07-07 21:03:28 -0400

    add thief class

---
+++ Source/ProjectRansack/Public/Thief.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base3C.h"
+#include "Thief.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRANSACK_API AThief : public ABase3C
+{
+	GENERATED_BODY()
+	
+};
