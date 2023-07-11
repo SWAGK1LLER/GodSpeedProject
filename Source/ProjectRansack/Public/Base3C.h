04dd93011c9000434657cbf88014a15e9c4c9e77
Daniel Haselock <daniel.haselock@gmail.com>
2023-07-10 22:30:08 -0400

    Fix shitty git merge

--- Source/ProjectRansack/Public/Base3C.h
+++ Source/ProjectRansack/Public/Base3C.h
@@ -17,11 +17,7 @@ public:
 	class USkeletalMeshComponent* skeletalMesh;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* cameraBoom;
-
-	/* Camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* camera;
+		class UCameraComp* cameraComponent;
 
 	/* Input */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
@@ -60,28 +56,6 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	UPROPERTY(editAnywhere, blueprintReadWrite)
-		class USkeletalMeshComponent* skeletalMesh;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-		class UCameraComp* cameraComponent;
-
-	/* Input */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-		class UInputMappingContext* inputHandler;
-
-	/**Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-		class UInputAction* moveAction;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-		class UInputAction* interactAction;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-		class UInputAction* lookAction;
-
-	/*Health Component*/
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Health, meta = (AllowPrivateAccess = "true"))
-		class UHealthComponent* healthComp;
 
 	/** Called for movement input */
 	void Move(const FInputActionValue& Value);
