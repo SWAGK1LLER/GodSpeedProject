e8e13464de8e05c0ce2378b4a174b22317b7a320
Gabriel Boisvert <gabriel.boisvert.dev@gmail.com>
2023-09-22 16:47:50 -0400

    V2 camera

--- Source/ProjectRansack/Private/CameraCompThief.cpp
+++ Source/ProjectRansack/Private/CameraCompThief.cpp
@@ -2,12 +2,18 @@
 #include "Camera/CameraComponent.h"
 #include <ProjectRansack/Public/Base3C.h>
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+bool SameSign(float x, float y)
+{
+	return ((x < 0) == (y < 0));
+}
 
 UCameraCompThief::UCameraCompThief()
 {
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	
-	CameraBoom->TargetArmLength = 400.0f;
+	CameraBoom->TargetArmLength = 300.0f;
 	CameraBoom->bUsePawnControlRotation = true;
 
 	// Create a follow camera
@@ -24,13 +30,29 @@ UCameraCompThief::UCameraCompThief()
 
 	// Configure character movement
 	owner->GetCharacterMovement()->bOrientRotationToMovement = true;
-	owner->GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+	owner->GetCharacterMovement()->RotationRate = FRotator(0.0f, 0.0f, 0.0f);
+}
+
+void UCameraCompThief::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	UCameraComp::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (ShouldTurn)
+	{
+		ACharacter* Character = CastChecked<ACharacter>(GetOwner());
+		Character->SetActorRotation(FMath::Lerp(Character->GetActorRotation(), finalRotation, 0.09f));
+
+		if (Character->GetActorRotation() == finalRotation)
+			ShouldTurn = false;
+	}
 }
 
 void UCameraCompThief::SetupCamera(USceneComponent* root)
 {
 	CameraBoom->SetupAttachment(root, "NeckSocket");
 	camera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+
+	rootMesh = root;
 }
 
 void UCameraCompThief::look(const FInputActionValue& Value)
@@ -40,5 +62,31 @@ void UCameraCompThief::look(const FInputActionValue& Value)
 	{
 		Character->AddControllerYawInput(LookAxisVector.X);
 		Character->AddControllerPitchInput(LookAxisVector.Y);
+		
+		RotatePlayer(Character);
+	}
+}
+
+void UCameraCompThief::RotatePlayer(ACharacter* Character)
+{
+	FVector CameraLocation = camera->GetComponentLocation();
+	FVector CameraForwardVector = camera->GetForwardVector();
+	FVector ToPlayerCharacter = Character->GetActorForwardVector();
+
+	CameraForwardVector.Normalize();
+	ToPlayerCharacter.Normalize();
+
+	if (FVector::DotProduct(CameraForwardVector, ToPlayerCharacter) < 0.5f)
+	{
+		ShouldTurn = true;
+
+		//To do : lerp rotation
+		FRotator rotation = Character->GetViewRotation();
+		rotation.Pitch = 0;
+		rotation.Roll = 0;
+
+		finalRotation = rotation;
+
+		//Character->SetActorRotation(rotation);
 	}
 }
--- Source/ProjectRansack/Public/CameraCompThief.h
+++ Source/ProjectRansack/Public/CameraCompThief.h
@@ -11,10 +11,20 @@ class PROJECTRANSACK_API UCameraCompThief : public UCameraComp
 	GENERATED_BODY()
 	
 public:
+	FRotator finalRotation;
+	bool ShouldTurn = false;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* CameraBoom = nullptr;
 
+	USceneComponent* rootMesh = nullptr;
+
 	UCameraCompThief();
+
+	void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 	void look(const FInputActionValue& Value) override;
 	void SetupCamera(USceneComponent* root) override;
+
+	void RotatePlayer(class ACharacter* Character);
 };
