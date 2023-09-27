bfc29958b3e082dc768a20bbd4d2b459cd3dee32
Gabriel Boisvert <142422550+gabrielbnvizzio@users.noreply.github.com>
2023-09-27 10:28:14 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/SWAGK1LLER/GodSpeedProject""
    This reverts commit cfca1f9fa859ae8e8dcc689e40a25ca32be8257f.

--- Source/ProjectRansack/ProjectRansack.Build.cs
+++ Source/ProjectRansack/ProjectRansack.Build.cs
@@ -8,6 +8,6 @@ public class ProjectRansack : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "OnlineSubsystem", "OnlineSubsystemEOS", "UMG", "SlateCore", "CommonUI" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "OnlineSubsystem", "OnlineSubsystemEOS", "UMG", "SlateCore" });
 	}
 }
